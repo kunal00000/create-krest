b2136c092c1d65896901c0aa52015f73f8d21b01
kunal00000 <kunalverma2468@gmail.com>
2023-08-13 18:32:54 +0530

    feat: add short documentation for route template

--- templates/template-basic-js/index.js
+++ templates/template-basic-js/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config");
@@ -11,10 +12,12 @@ app.use(bodyParser.json());
 app.use(cors({ origin: "*" })); // default: allows all origins to access the server
 
 // * routes
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello World!" });
-});
 app.use("/todos", todoRouter);
+
+app.use(express.static("public"));
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
